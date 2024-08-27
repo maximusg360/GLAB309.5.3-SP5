8b83fb918162490787e64ea4822d7efe61645558
Guzman <francisco.guzman@accenture.com>
2024-08-27 14:34:47 -0400

    first commit

---
+++ src/main/java/com/example/modeldemo/ModeldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.modeldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeldemoApplication.class, args);
+	}
+
+}
